29ef9ebdbac7f803424582b265e698da9455f2fa
ajaykamble4241 <54786628+ajaykamble4241@users.noreply.github.com>
2024-07-30 15:26:36 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -113,23 +113,23 @@ elif menu == "Order Management":
         if submit_button:
             create_order(order_id, customer_id, order_date, status)
             st.success(f"Order added successfully!")
-        elif action == "Update Order":
-            with st.form("update_product"):
-                order_id = st.text_input("Order ID")
-                customer_id = st.text_input("Customer ID")
-                order_date = st.st.date_input("Select date", datetime.now())
-                status = st.selectbox("Select Option of Status", ("Completed", "Pending", "Shipped"))
-                submit_button = st.form_submit_button(label="Add Order")
-            if submit_button:
-                update_order(order_id, customer_id, order_date, status)
-                st.success(f"Order updated successfully!")
+    elif action == "Update Order":
+        with st.form("update_product"):
+            order_id = st.text_input("Order ID")
+            customer_id = st.text_input("Customer ID")
+            order_date = st.st.date_input("Select date", datetime.now())
+            status = st.selectbox("Select Option of Status", ("Completed", "Pending", "Shipped"))
+            submit_button = st.form_submit_button(label="Add Order")
+        if submit_button:
+            update_order(order_id, customer_id, order_date, status)
+            st.success(f"Order updated successfully!")
         elif action == "Delete Order":
             with st.form("delete_product"):
-                order_id = st.text_input("Order ID")
-                submit_button = st.form_submit_button(label="Delete Product")
-                if submit_button:
-                    delete_product(order_id)
-                    st.success(f"Order {order_id} deleted successfully!")
+            order_id = st.text_input("Order ID")
+            submit_button = st.form_submit_button(label="Delete Product")
+            if submit_button:
+                delete_product(order_id)
+                st.success(f"Order {order_id} deleted successfully!")
 
 
 # Order and Product Reports
